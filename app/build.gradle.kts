d9f8cf9b5de32ffa83268540f2efb21b0588af10
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-30 23:52:04 -0300

    Bump version for radar mode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
