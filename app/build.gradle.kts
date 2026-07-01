fd7c972da148a0d3c9ce559fb754339556828351
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-30 22:57:53 -0300

    Bump app version for Google Maps geocoding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
