a9885f5ff1dc7b120b47111282195889b7f1412d
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-04 08:20:41 -0300

    Define navigation constants for app shortcuts

---
+++ app/src/main/java/br/com/mapeiaia/rotacerta/NavigationConstants.kt
@@ -0,0 +1,8 @@
+package br.com.mapeiaia.rotacerta
+
+const val TAB_ANALYSIS = "analysis"
+const val TAB_CONFIG = "config"
+const val TAB_HISTORY = "history"
+
+const val EXTRA_OPEN_TAB = "open_tab"
+const val EXTRA_SAVED_PLACE_ID = "saved_place_id"
