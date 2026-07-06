153ac5e77d0b4f45d6d4128128f5355f70aa1f6a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-06 12:48:49 -0300

    Fix coroutine play services dependency coordinate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,7 +99,7 @@ dependencies {
     implementation("com.google.mlkit:text-recognition:16.0.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
-    implementation("org.jetbrains.kotlinx-coroutines-play-services:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
     testImplementation("junit:junit:4.13.2")
