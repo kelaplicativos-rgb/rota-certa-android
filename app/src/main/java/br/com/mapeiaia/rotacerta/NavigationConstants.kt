36468508b09ca297205c675eb2b55371fc648d75
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-06 11:49:57 -0300

    Add config section navigation constants

--- app/src/main/java/br/com/mapeiaia/rotacerta/NavigationConstants.kt
+++ app/src/main/java/br/com/mapeiaia/rotacerta/NavigationConstants.kt
@@ -7,3 +7,5 @@ const val TAB_HISTORY = "history"
 
 const val EXTRA_OPEN_TAB = "open_tab"
 const val EXTRA_SAVED_PLACE_ID = "saved_place_id"
+const val EXTRA_OPEN_CONFIG_SECTION = "open_config_section"
+const val CONFIG_SECTION_DECISION_ADDRESS = "decision_address"
