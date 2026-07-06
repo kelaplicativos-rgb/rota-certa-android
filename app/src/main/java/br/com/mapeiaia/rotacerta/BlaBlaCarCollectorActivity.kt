a69064d5d562a84f3b94b2ba05e15a4267fdca4d
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-05 23:26:40 -0300

    Fix BlaBlaCar collector imports

--- app/src/main/java/br/com/mapeiaia/rotacerta/BlaBlaCarCollectorActivity.kt
+++ app/src/main/java/br/com/mapeiaia/rotacerta/BlaBlaCarCollectorActivity.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DarkColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
