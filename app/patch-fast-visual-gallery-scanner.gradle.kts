7a70713cd26859e8edb1e3e153f749fa5dd6ef05
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-08 16:16:36 -0300

    Bump visual scanner patch version to 0.1.61

--- app/patch-fast-visual-gallery-scanner.gradle.kts
+++ app/patch-fast-visual-gallery-scanner.gradle.kts
@@ -1,3 +1,7 @@
+extensions.configure<com.android.build.gradle.BaseExtension>("android") {
+    defaultConfig.versionName = "0.1.61"
+}
+
 val fastVisualGalleryScanner by tasks.registering {
     val serviceFile = layout.projectDirectory.file("src/main/java/br/com/mapeiaia/rotacerta/LiveRideAccessibilityService.kt")
     inputs.file(serviceFile)
