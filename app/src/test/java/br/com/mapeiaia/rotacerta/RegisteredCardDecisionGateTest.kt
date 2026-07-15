2a54e593d9b3905cc0d6d8231e05bfe8a16443b3
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-15 11:17:11 -0300

    Test immediate rejection of sticky decision windows

--- app/src/test/java/br/com/mapeiaia/rotacerta/RegisteredCardDecisionGateTest.kt
+++ app/src/test/java/br/com/mapeiaia/rotacerta/RegisteredCardDecisionGateTest.kt
@@ -28,24 +28,42 @@ class RegisteredCardDecisionGateTest {
     }
 
     @Test
-    fun stickyDecisionIsActiveOnlyAfterRegisteredCardWasRecentlySeen() {
+    fun naturalFreshnessWindowStillWorks() {
         var now = 20_000L
         val gate = RegisteredCardDecisionGate(nowProvider = { now })
 
-        assertFalse(gate.hasSeenRecently(maxAgeMillis = 3_500L))
-
         gate.markSeen()
-        now += 3_499L
+        now += 349L
 
-        assertTrue(gate.hasSeenRecently(maxAgeMillis = 3_500L))
+        assertTrue(gate.hasSeenRecently())
 
         now += 1L
 
+        assertFalse(gate.hasSeenRecently())
+    }
+
+    @Test
+    fun callerCannotExtendFreshnessAndKeepPreviousDecisionOnScreen() {
+        var now = 30_000L
+        val gate = RegisteredCardDecisionGate(nowProvider = { now })
+
+        gate.markSeen()
+
+        assertFalse(gate.hasSeenRecently(maxAgeMillis = 2_800L))
         assertFalse(gate.hasSeenRecently(maxAgeMillis = 3_500L))
+
+        now += 50L
+
+        assertFalse(gate.hasSeenRecently(maxAgeMillis = 2_800L))
+    }
+
+    @Test
+    fun clearAlwaysInvalidatesFreshness() {
+        val gate = RegisteredCardDecisionGate()
 
         gate.markSeen()
         gate.clear()
 
-        assertFalse(gate.hasSeenRecently(maxAgeMillis = 3_500L))
+        assertFalse(gate.hasSeenRecently())
     }
 }
