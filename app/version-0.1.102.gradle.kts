e4cfdda3e5f214e059c03c5d17778a20938ce6bc
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-17 09:27:31 -0300

    Fix version script compatibility for 0.1.102

--- app/version-0.1.102.gradle.kts
+++ app/version-0.1.102.gradle.kts
@@ -1,5 +1,7 @@
-extensions.configure<com.android.build.api.dsl.ApplicationExtension>("android") {
-    defaultConfig {
-        versionName = "0.1.102"
-    }
-}
+val androidExtension = extensions.getByName("android")
+val defaultConfig = androidExtension.javaClass.methods
+    .first { method -> method.name == "getDefaultConfig" && method.parameterCount == 0 }
+    .invoke(androidExtension)
+defaultConfig.javaClass.methods
+    .first { method -> method.name == "setVersionName" && method.parameterCount == 1 }
+    .invoke(defaultConfig, "0.1.102")
