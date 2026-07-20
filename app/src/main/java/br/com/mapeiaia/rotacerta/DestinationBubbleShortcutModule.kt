18275a7cbe90b48327f14f8a3153efd480e0f43e
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-20 14:15:20 -0300

    Add destination shortcut module

---
+++ app/src/main/java/br/com/mapeiaia/rotacerta/DestinationBubbleShortcutModule.kt
@@ -0,0 +1,12 @@
+package br.com.mapeiaia.rotacerta
+
+object DestinationBubbleShortcutModule : BubbleShortcutModule {
+    override val spec = BubbleShortcutSpec(
+        id = "destination",
+        emoji = "🏠",
+        label = "Destino",
+        action = BubbleShortcutAction.OpenDestination,
+        targetGroup = "destination",
+        targetTab = "analysis",
+    )
+}
