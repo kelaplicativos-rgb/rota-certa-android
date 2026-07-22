df86b9ff90dd617a02aeeb75ba2787fd3d895fd9
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-21 21:11:04 -0300

    fix: aplicar versão 0.1.127 pelo padrão compatível

--- app/version-0.1.127.gradle.kts
+++ app/version-0.1.127.gradle.kts
@@ -1,7 +1,7 @@
-// Rota Certa 0.1.127
-// Restaura selecao manual de apps e cards opcionais, sem qualquer pre-cadastro.
-android {
-    defaultConfig {
-        versionName = "0.1.127"
-    }
-}
+val androidExtension127 = extensions.getByName("android")
+val defaultConfig127 = androidExtension127.javaClass.methods
+    .first { method -> method.name == "getDefaultConfig" && method.parameterCount == 0 }
+    .invoke(androidExtension127)
+defaultConfig127.javaClass.methods
+    .first { method -> method.name == "setVersionName" && method.parameterCount == 1 }
+    .invoke(defaultConfig127, "0.1.127")
