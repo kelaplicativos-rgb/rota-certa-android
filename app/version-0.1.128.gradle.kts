ed1208ea610665617444927176ce5ca2c0ce7b73
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-07-22 11:41:01 -0300

    build: add version 0.1.128 finalizer

---
+++ app/version-0.1.128.gradle.kts
@@ -0,0 +1,10 @@
+val androidExtension128 = extensions.getByName("android")
+val defaultConfig128 = androidExtension128.javaClass.methods
+    .first { method -> method.name == "getDefaultConfig" && method.parameterCount == 0 }
+    .invoke(androidExtension128)
+defaultConfig128.javaClass.methods
+    .first { method -> method.name == "setVersionName" && method.parameterCount == 1 }
+    .invoke(defaultConfig128, "0.1.128")
+
+// Ultimo patch da cadeia: executa depois do contrato estrito 0.1.127.
+apply(from = "fast-cache-screenoff-0.1.128.gradle.kts")
