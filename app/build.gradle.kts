167aca3e4a1eecb01f7d37a85da71eb8c39bf89f
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-20 20:41:12 -0300

    chore(stage47): bump MHTML manual-seat candidate to 0.1.242

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5534
-        versionName = "0.1.241"
+        versionCode = 5535
+        versionName = "0.1.242"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
