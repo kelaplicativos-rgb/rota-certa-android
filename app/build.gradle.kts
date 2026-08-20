719af8891ae669f6b9a14b654ac1400b660c1c03
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-20 15:50:15 -0300

    Stage47 bump direct-source physical candidate to 0.1.239

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5531
-        versionName = "0.1.238"
+        versionCode = 5532
+        versionName = "0.1.239"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
