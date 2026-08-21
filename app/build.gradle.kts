3484d69d25e352b91f86a48fe23e9b1d0c36adc8
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-20 22:07:40 -0300

    chore(stage47): bump navigation fix candidate to 0.1.244

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5536
-        versionName = "0.1.243"
+        versionCode = 5537
+        versionName = "0.1.244"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
