5077cdc21029735fa402153e78e84a67e94c89fe
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-22 01:56:10 -0300

    Fix Compose RowScope weight compile error

--- app/src/main/java/br/com/mapeiaia/rotacerta/trips/PassengerTimelineUi.kt
+++ app/src/main/java/br/com/mapeiaia/rotacerta/trips/PassengerTimelineUi.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
