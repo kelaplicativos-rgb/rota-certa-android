3f3a8c9915708a25bffb57badab2cbce21a3d9d0
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-22 12:50:55 -0300

    Bump passenger card actions to 0.1.257

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5549
-        versionName = "0.1.256"
+        versionCode = 5550
+        versionName = "0.1.257"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
