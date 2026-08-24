abb6e080eaae672b292b4d99a07e17f2716b0b48
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-24 20:32:30 -0300

    build(agenda): version global trace APK 0.1.273

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5565
-        versionName = "0.1.272"
+        versionCode = 5566
+        versionName = "0.1.273"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
