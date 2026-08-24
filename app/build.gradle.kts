ec14239b6b454d7974d55532dc86d0d31fbb51b6
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-24 14:02:46 -0300

    chore(app): bump timeline clear candidate to 0.1.270

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5562
-        versionName = "0.1.269"
+        versionCode = 5563
+        versionName = "0.1.270"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
