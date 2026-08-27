961e95133b6aef06d01245301d23450c980322e8
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-27 00:22:15 -0300

    chore(agenda): bump automatic public agenda sync to 0.1.300

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5592
-        versionName = "0.1.299"
+        versionCode = 5593
+        versionName = "0.1.300"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
