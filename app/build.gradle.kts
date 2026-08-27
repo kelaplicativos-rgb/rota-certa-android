f55e07be11137c8906ce626925386808c8ff719d
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-27 13:23:23 -0300

    build: bump Rota Certa to 0.1.309 5602

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5601
-        versionName = "0.1.308"
+        versionCode = 5602
+        versionName = "0.1.309"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
