68798d1a042efc69a4894306955a802ca7667e53
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-27 00:39:58 -0300

    chore: bump seat regression fix to 0.1.301

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5593
-        versionName = "0.1.300"
+        versionCode = 5594
+        versionName = "0.1.301"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
