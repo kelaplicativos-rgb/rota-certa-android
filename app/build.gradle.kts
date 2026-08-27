65eed9d87224eda3a32d3eb37fc9d354a4d38823
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-27 09:12:07 -0300

    build: bump agenda link actions to 0.1.306

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5598
-        versionName = "0.1.305"
+        versionCode = 5599
+        versionName = "0.1.306"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
