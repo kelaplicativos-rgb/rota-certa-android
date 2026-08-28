969b5705707a2e90019fc2e7d1586d14e382f225
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-28 16:47:20 -0300

    chore(blablacar): bump regex fix build to 0.1.325

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5617
-        versionName = "0.1.324"
+        versionCode = 5618
+        versionName = "0.1.325"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
