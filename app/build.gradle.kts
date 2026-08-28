34038e46e2f9c8e4a0178460c72743b4d3b541b4
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-28 01:20:53 -0300

    build: start modular browser orchestrator 0.1.318

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5610
-        versionName = "0.1.317"
+        versionCode = 5611
+        versionName = "0.1.318"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
