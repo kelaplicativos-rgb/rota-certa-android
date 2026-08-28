6b783e50ccfc6de8b515a5788d3532952113be18
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-28 13:28:49 -0300

    chore(agenda): bump BlaBla sync recovery to 0.1.320

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5612
-        versionName = "0.1.319"
+        versionCode = 5613
+        versionName = "0.1.320"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
