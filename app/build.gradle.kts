f9a5a812e459dc1c15e8af3dbe79a87226839202
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-29 11:55:52 -0300

    chore(agenda): bump date-field UX build to 0.1.335

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5627
-        versionName = "0.1.334"
+        versionCode = 5628
+        versionName = "0.1.335"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
