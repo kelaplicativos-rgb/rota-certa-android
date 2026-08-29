49a3bbf233baa12c3e32f02a99167c134f2a2503
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-29 13:35:26 -0300

    chore(date): bump global date contract build to 0.1.340

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5632
-        versionName = "0.1.339"
+        versionCode = 5633
+        versionName = "0.1.340"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
