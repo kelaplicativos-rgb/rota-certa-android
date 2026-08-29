878f577eedd26896c0982df3f9cf3dc5579400d2
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-29 10:52:49 -0300

    chore(agenda): bump public-search today-start build to 0.1.332

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5624
-        versionName = "0.1.331"
+        versionCode = 5625
+        versionName = "0.1.332"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
