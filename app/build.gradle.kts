0bff3b14296448379c485f7a21a40dde78731e82
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-28 23:10:15 -0300

    chore(blablacar): bump sequential sync gate to 0.1.327

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5619
-        versionName = "0.1.326"
+        versionCode = 5620
+        versionName = "0.1.327"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
