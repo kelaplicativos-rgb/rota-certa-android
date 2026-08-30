e21f3da3663fe40b0030fd900fbad8736f586037
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-30 18:14:13 -0300

    build: bump capture orchestrator to 0.1.368

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5660
-        versionName = "0.1.367"
+        versionCode = 5661
+        versionName = "0.1.368"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
