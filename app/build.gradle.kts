8ac4891d3b4d5ecfea84d4adc74b779b69bb5670
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-30 11:22:43 -0300

    chore(agenda): bump real-seat flow to 0.1.357

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5649
-        versionName = "0.1.356"
+        versionCode = 5650
+        versionName = "0.1.357"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
