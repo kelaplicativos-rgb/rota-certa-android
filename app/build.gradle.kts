a7c850620b198087f53dbc8070477fff759c7ff7
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-30 21:30:08 -0300

    build: bump direct free-seat Agenda fix to 0.1.371

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5663
-        versionName = "0.1.370"
+        versionCode = 5664
+        versionName = "0.1.371"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
