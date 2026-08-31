2000ee6d8cf5d1337b100f1ccd31a57dd83fb355
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-08-30 23:56:19 -0300

    chore(build): bump public sync fix to 0.1.373

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5665
-        versionName = "0.1.372"
+        versionCode = 5666
+        versionName = "0.1.373"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
