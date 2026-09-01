c51ec680774ace2b2cde3db4c66a56bef12fdfbc
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-01 07:57:24 -0300

    fix(agenda): use PointerInputChange member consume API

--- app/src/main/java/br/com/mapeiaia/rotacerta/trips/AgendaPullRefreshGesture0388.kt
+++ app/src/main/java/br/com/mapeiaia/rotacerta/trips/AgendaPullRefreshGesture0388.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalViewConfiguration
 import androidx.compose.ui.unit.dp
