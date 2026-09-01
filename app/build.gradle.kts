ef3dd72163739691812904955cbe294958c6cf0d
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-01 09:20:01 -0300

    test(agenda): make AndroidJUnitRunner dependency explicit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -167,6 +167,7 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
