21894443458ad66e239f9f207bedd4ff2b36f9fa
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-01 12:04:09 -0300

    chore(app): bump Agenda to 0.1.389

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5681
-        versionName = "0.1.388"
+        versionCode = 5682
+        versionName = "0.1.389"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
