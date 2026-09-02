21f8696b2986916a2bd4914c6e5ff14fdd61e937
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-02 01:21:08 -0300

    chore(android): bump headless callback fix to 0.1.405

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5697
-        versionName = "0.1.404"
+        versionCode = 5698
+        versionName = "0.1.405"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
