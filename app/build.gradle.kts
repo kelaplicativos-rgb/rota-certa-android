6fae9c89159b4c88037648934dd4689cf038c16a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-02 00:12:23 -0300

    chore(app): bump canonical central sync to 0.1.403

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5695
-        versionName = "0.1.402"
+        versionCode = 5696
+        versionName = "0.1.403"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
