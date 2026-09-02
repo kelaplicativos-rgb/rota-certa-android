e13304b1be7262e33a7e0d1776ba60cb3ad3dfff
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-02 08:25:21 -0300

    chore(android): identify BlaBlaCar public-link build 0.1.409

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5701
-        versionName = "0.1.408"
+        versionCode = 5702
+        versionName = "0.1.409"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
