ce75ec462337ca505ee866230519c8b8094c5203
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-02 16:49:22 -0300

    chore(version): bump timeline clean surface to 0.1.415

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5707
-        versionName = "0.1.414"
+        versionCode = 5708
+        versionName = "0.1.415"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
