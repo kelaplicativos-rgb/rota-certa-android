b493789f98a8ec744521982a63c1abcf4936d147
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-02 17:44:12 -0300

    build: include Material notification icon set

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,7 +148,7 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.15.0")
