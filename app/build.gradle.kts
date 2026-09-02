8ddbaa015075f78e5fcb76602fbdb193e5ca41ce
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-02 01:57:52 -0300

    build: bump canonical integrity to 0.1.406

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5698
-        versionName = "0.1.405"
+        versionCode = 5699
+        versionName = "0.1.406"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
