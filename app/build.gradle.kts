f3adc9c92f500b320dc0cc3479741d270ab37254
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-03 15:59:37 -0300

    build(agenda): bump independent mirror fix to 0.1.437

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5729
-        versionName = "0.1.436"
+        versionCode = 5730
+        versionName = "0.1.437"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
