6d2b8e3e4f0bee76eae39b33e8cc07a30a558a2b
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-02 22:25:08 -0300

    chore: bump Agenda readback forensics to 0.1.421

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5713
-        versionName = "0.1.420"
+        versionCode = 5714
+        versionName = "0.1.421"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
