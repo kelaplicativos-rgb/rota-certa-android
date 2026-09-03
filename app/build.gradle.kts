a330e826e2884c152b404e1d917e0590964c1894
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-02 21:59:25 -0300

    chore(version): bump passenger admin session fix to 0.1.420

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5712
-        versionName = "0.1.419"
+        versionCode = 5713
+        versionName = "0.1.420"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
