c76881e9e45f2ac583eca81cef596556a0aadda1
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-04 10:21:17 -0300

    build: preparar APK 0.1.452

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5744
-        versionName = "0.1.451"
+        versionCode = 5745
+        versionName = "0.1.452"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
