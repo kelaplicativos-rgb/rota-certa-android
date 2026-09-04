44db9242f3c4fbef0ca0d16d46f810db26364782
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-04 20:48:37 -0300

    build: versão 0.1.460 replay canônico durável

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5752
-        versionName = "0.1.459"
+        versionCode = 5753
+        versionName = "0.1.460"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
