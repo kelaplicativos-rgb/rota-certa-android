9746958d9f1d129a2b4d33e188f0e73040b7fa28
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-03 21:45:40 -0300

    fix(blablacar): rebuild public URL observer cleanly 0.1.446

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5738
-        versionName = "0.1.445"
+        versionCode = 5739
+        versionName = "0.1.446"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
