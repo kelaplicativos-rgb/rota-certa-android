f43ee66a4e208196cb9d0863c696ad3cab8aa970
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-04 19:13:33 -0300

    build: versão 0.1.459 evidência por tentativa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5751
-        versionName = "0.1.458"
+        versionCode = 5752
+        versionName = "0.1.459"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
