086ac45440fc366f4ec3b1a573f5faa55ba07334
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-05 08:28:28 -0300

    build: versao 0.1.465 verde URL manual

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5757
-        versionName = "0.1.464"
+        versionCode = 5758
+        versionName = "0.1.465"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
