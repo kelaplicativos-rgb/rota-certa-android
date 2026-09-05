2f7b10a914f1122a7d4bc85fbb8258afa6181392
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-05 00:32:33 -0300

    build: versão 0.1.463 recuperação de verificação órfã

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5755
-        versionName = "0.1.462"
+        versionCode = 5756
+        versionName = "0.1.463"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
