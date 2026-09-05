19b49e006d71eb2a2bea84aa05990e58b76f41ac
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-04 23:39:30 -0300

    build: versão 0.1.462 share público autoritativo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5754
-        versionName = "0.1.461"
+        versionCode = 5755
+        versionName = "0.1.462"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
