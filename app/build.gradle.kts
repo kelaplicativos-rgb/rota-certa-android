3e91884b44f9189f9bfd2ef8b5a86b0f950c2f91
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-05 09:21:58 -0300

    build: versao 0.1.466 visibilidade real da Agenda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5758
-        versionName = "0.1.465"
+        versionCode = 5759
+        versionName = "0.1.466"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
