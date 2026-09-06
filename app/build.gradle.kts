b9490b44c7e41d2de3b41083df74b043431efc28
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-06 14:56:39 -0300

    build: bump post-build fix to 0.1.493

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5784
-        versionName = "0.1.492"
+        versionCode = 5785
+        versionName = "0.1.493"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
