822be710e9e7bece567b393696c865759386c0d3
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-07 00:03:05 -0300

    build: bump canonical Timeline to 0.1.494

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5785
-        versionName = "0.1.493"
+        versionCode = 5786
+        versionName = "0.1.494"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
