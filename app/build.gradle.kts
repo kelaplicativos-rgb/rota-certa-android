7aa678f742679f218ab06be3f9ca5b3b32e9516f
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-07 23:07:56 -0300

    chore(android): bump Agenda revision-rebase build to 0.1.502

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5793
-        versionName = "0.1.501"
+        versionCode = 5794
+        versionName = "0.1.502"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
