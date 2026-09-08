612a67657b06392aaeacb7f698735e6f7eb2c601
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-08 00:51:41 -0300

    chore(android): bump numeric vacancy fix to 0.1.504

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5795
-        versionName = "0.1.503"
+        versionCode = 5796
+        versionName = "0.1.504"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
