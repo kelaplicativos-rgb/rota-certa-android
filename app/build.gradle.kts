a800ad0ec579cfed755491abc8257ca54fa4c4fe
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-09 23:41:32 -0300

    build: bump Rota Certa to forensic 0.1.528

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5819
-        versionName = "0.1.527"
+        versionCode = 5820
+        versionName = "0.1.528"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
