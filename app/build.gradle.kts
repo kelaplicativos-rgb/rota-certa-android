59a883bacc0b4ae3d0a72a598b5820c995d67248
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-10 07:28:51 -0300

    chore(android): bump portable rides JSON build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5822
-        versionName = "0.1.530"
+        versionCode = 5823
+        versionName = "0.1.531"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
