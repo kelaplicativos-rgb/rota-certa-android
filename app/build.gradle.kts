03a0705ac1a926cb5c1d8b5dce85aead05d7da9a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-09 21:07:49 -0300

    chore(android): bump Timeline card recovery to 0.1.524

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "br.com.mapeiaia.rotacerta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5815
-        versionName = "0.1.523"
+        versionCode = 5816
+        versionName = "0.1.524"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"${googleMapsApiKey.escapeForBuildConfig()}\"")
