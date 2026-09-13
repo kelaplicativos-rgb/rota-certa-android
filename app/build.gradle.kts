411fd5dcc3bfce1c2d99922976a120a2eb26814f
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-12 22:19:59 -0300

    build(version-center): make Google Services config optional for source validation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,12 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
-    id("com.google.gms.google-services")
+    id("com.google.gms.google-services") apply false
+}
+
+val googleServicesConfigFile = layout.projectDirectory.file("google-services.json").asFile
+if (googleServicesConfigFile.isFile) {
+    apply(plugin = "com.google.gms.google-services")
 }
 
 val localProperties = Properties().apply {
