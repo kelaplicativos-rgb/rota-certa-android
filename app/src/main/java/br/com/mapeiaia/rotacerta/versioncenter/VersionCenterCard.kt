64fce2ec2ffe6ea0d7224fc6b8e69e1212cfcbfe
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-12 21:43:08 -0300

    fix(version-center): restore Compose weight import

--- app/src/main/java/br/com/mapeiaia/rotacerta/versioncenter/VersionCenterCard.kt
+++ app/src/main/java/br/com/mapeiaia/rotacerta/versioncenter/VersionCenterCard.kt
@@ -11,7 +11,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.rememberScrollState
+import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Card
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.MaterialTheme
