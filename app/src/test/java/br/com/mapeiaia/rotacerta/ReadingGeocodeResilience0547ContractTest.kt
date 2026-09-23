beef490b559915686e74a2ecd594640f45715854
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-23 20:00:04 -0300

    test(release): advance geocode resilience contract to 0.1.642

--- app/src/test/java/br/com/mapeiaia/rotacerta/ReadingGeocodeResilience0547ContractTest.kt
+++ app/src/test/java/br/com/mapeiaia/rotacerta/ReadingGeocodeResilience0547ContractTest.kt
@@ -63,7 +63,7 @@ class ReadingGeocodeResilience0547ContractTest {
     @Test
     fun version_is_0547() {
         val build = source("build.gradle.kts")
-        assertTrue(build.contains("releaseVersionCode = 5_932"))
-        assertTrue(build.contains("releaseVersionName = \"0.1.641\""))
+        assertTrue(build.contains("releaseVersionCode = 5_933"))
+        assertTrue(build.contains("releaseVersionName = \"0.1.642\""))
     }
 }
