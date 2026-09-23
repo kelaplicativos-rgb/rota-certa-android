ec5a8b2506c60445c9d5cf28209d4e1e5087d437
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-23 20:32:39 -0300

    chore(farol): remove obsolete MemorizeFarolCard action enum

--- app/src/main/java/br/com/mapeiaia/rotacerta/BubbleShortcutModule.kt
+++ app/src/main/java/br/com/mapeiaia/rotacerta/BubbleShortcutModule.kt
@@ -28,7 +28,6 @@ enum class BubbleShortcutAction {
     OpenAuthorizedAppsAndCards,
     OpenTextCorrection,
     SaveScreenPrint,
-    MemorizeFarolCard,
 }
 
 enum class BubbleShortcutQuickAction {
