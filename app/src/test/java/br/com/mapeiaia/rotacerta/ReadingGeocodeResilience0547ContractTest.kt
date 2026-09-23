cef53471ead1f91d44535b616a19414c3bab55e4
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-23 13:31:43 -0300

    test(0.1.636): advance geocode release metadata contract

--- app/src/test/java/br/com/mapeiaia/rotacerta/ReadingGeocodeResilience0547ContractTest.kt
+++ app/src/test/java/br/com/mapeiaia/rotacerta/ReadingGeocodeResilience0547ContractTest.kt
@@ -63,7 +63,7 @@ class ReadingGeocodeResilience0547ContractTest {
     @Test
     fun version_is_0547() {
         val build = source("build.gradle.kts")
-        assertTrue(build.contains("releaseVersionCode = 5_926"))
-        assertTrue(build.contains("releaseVersionName = \"0.1.635\""))
+        assertTrue(build.contains("releaseVersionCode = 5_927"))
+        assertTrue(build.contains("releaseVersionName = \"0.1.636\""))
     }
 }
