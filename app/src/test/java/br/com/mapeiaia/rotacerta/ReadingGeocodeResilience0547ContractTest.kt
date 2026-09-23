cf39988a00c77b5423730c9d24e5a938daedfdc3
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-09-23 16:16:40 -0300

    test(release): advance current build contract to 0.1.638

--- app/src/test/java/br/com/mapeiaia/rotacerta/ReadingGeocodeResilience0547ContractTest.kt
+++ app/src/test/java/br/com/mapeiaia/rotacerta/ReadingGeocodeResilience0547ContractTest.kt
@@ -63,7 +63,7 @@ class ReadingGeocodeResilience0547ContractTest {
     @Test
     fun version_is_0547() {
         val build = source("build.gradle.kts")
-        assertTrue(build.contains("releaseVersionCode = 5_928"))
-        assertTrue(build.contains("releaseVersionName = \"0.1.637\""))
+        assertTrue(build.contains("releaseVersionCode = 5_929"))
+        assertTrue(build.contains("releaseVersionName = \"0.1.638\""))
     }
 }
